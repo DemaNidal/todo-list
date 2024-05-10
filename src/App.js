3647ed2e373141bfe0b9ee3d7d166c378c23dd31
DemaNidal <khalilidema@gmail.com>
2024-05-10 16:35:13 +0300

    creat modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoList from './components/TodoList';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-
+      <TodoList />
     </div>
   );
 }
